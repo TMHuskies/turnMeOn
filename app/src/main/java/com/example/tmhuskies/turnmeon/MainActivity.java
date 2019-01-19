442476c3d275b5fdaccbcc4969ad200a2c76008d
TravisCJY <travisching007@gmail.com>
2019-01-20 02:59:44 +0800

    Linked deactivate button

--- app/src/main/java/com/example/tmhuskies/turnmeon/MainActivity.java
+++ app/src/main/java/com/example/tmhuskies/turnmeon/MainActivity.java
@@ -14,7 +14,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.io.File;
@@ -99,6 +98,15 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
                         Toast.LENGTH_LONG).show();
                 runRecognizerSetup(code, audioPath);
 
+            }
+        });
+
+        // Listen from Deactivate button
+        Button deactivate = findViewById(R.id.deactivateButton);
+        deactivate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deactivateApp();
             }
         });
     }
