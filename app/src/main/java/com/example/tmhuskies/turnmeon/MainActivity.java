93f2aa6f5b28c48c1fd3312c2d1ee9d4aa51c1e8
TravisCJY <travisching007@gmail.com>
2019-01-19 17:36:04 +0800

    Link buttons in MainActivity to their activities

--- app/src/main/java/com/example/tmhuskies/turnmeon/MainActivity.java
+++ app/src/main/java/com/example/tmhuskies/turnmeon/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.tmhuskies.turnmeon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Listen to Trigger Button
+        Button trigger = findViewById(R.id.triggerButton);
+        trigger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TriggerActivity.class));
+            }
+        });
+
+        // Listen to Response Button
+        Button response = findViewById(R.id.responseButton);
+        response.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ResponseActivity.class));
+            }
+        });
     }
+
+
 }
