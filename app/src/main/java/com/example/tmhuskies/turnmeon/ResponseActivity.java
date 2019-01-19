b090437c2cb160729d7203a972d55100860ad581
kengwoon <shannalimkengwoon@gmail.com>
2019-01-19 17:49:02 +0800

    confirm button added to trigger activity

--- app/src/main/java/com/example/tmhuskies/turnmeon/ResponseActivity.java
+++ app/src/main/java/com/example/tmhuskies/turnmeon/ResponseActivity.java
@@ -2,12 +2,20 @@ package com.example.tmhuskies.turnmeon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class ResponseActivity extends AppCompatActivity {
+
+    private RecyclerView mUserResponseList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_response);
+
+        mUserResponseList = (RecyclerView)findViewById(R.id.response_list);
+        mUserResponseList.setHasFixedSize(true);
+        mUserResponseList.setLayoutManager(new LinearLayoutManager(this));
     }
 }
